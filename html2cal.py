6002b01438c52aefb4d4266e9266ceb9aeb7cb97
Bert <bert.claesen+github@gmail.com>
2019-09-12 20:57:05 +0200

    Updating for changes in 2019-2020 season
    * Correctly parse year
    * Tidy up the teamname and correctly select Real again

--- html2cal.py
+++ html2cal.py
@@ -9,10 +9,13 @@ from datetime  import datetime, timedelta
 from icalendar import Calendar, Event
 import pytz
 
+def tidyR(s):
+        return s.replace("(R)", "").strip()
+
 ###
 ###Get the matches
 ###
-doc = open("kalender.html")
+doc = open("competitiekalender2019.html")
 soup = BeautifulSoup(doc, 'html.parser')
 soup.find_all('a')
 #elke tr met 6 kinderen
@@ -24,14 +27,17 @@ for t in trs:
         match = {}
         match["Date"] = cells[1].text
         match["Location"] = cells[2].text
-        match["Game"] = cells[3].text + "-" + cells[5].text
+        match["Game"] = tidyR(cells[3].text) + "-" + tidyR(cells[5].text)
+
+        #match["Game"] = match["Game"].replace("(R)", "")
+        #match["Game"] = match["Game"].strip()
         matches.append(match)
         
    
 ###
 ### Filter the matches
 ###
-matches = [m for m in matches if m["Game"].find("R. Kiewit") != -1 and m["Game"].find("bye ")==-1]
+matches = [m for m in matches if m["Game"].find(tidyR("REAL KIEWIT  (R) (R)")) != -1 and m["Game"].find("bye ")==-1]
            
 #for m in matches:
 #    print("{}: {} // {}".format(m['Date'],m['Game'],m['Location']))
@@ -42,18 +48,14 @@ matches = [m for m in matches if m["Game"].find("R. Kiewit") != -1 and m["Game"]
 #29 okt 2017 is DST terug weg
 #25 maart 2018 is DST er weer
 cal = Calendar()
-fmt = "%d/%m/%y  %H:%M"
+fmt = "%d/%m/%Y %H:%M"
 tzone = pytz.timezone("Europe/Brussels")
-dststart = datetime(2017,10,29,2)
-dstend = datetime(2018,3,25,2)
+dststart = datetime(2018,10,28,2)
+dstend = datetime(2019,3,31,2)
 for m in matches:
     event = Event()
     event.add('summary', m["Game"])
     dtstart = datetime.strptime(m["Date"],fmt)  
-    #This math only works because I am running this in Brussels with DST
-    #if (dtstart-dststart) >= timedelta(hours=0) and (dstend-dtstart >= timedelta(hours=0)):
-    #    dtstart += timedelta(hours=1)
-    #dtstart -= timedelta(hours=1)
     dtstart = tzone.localize(dtstart)
     dtend = dtstart + timedelta(0,60*60)
     event.add('dtstart', dtstart)
