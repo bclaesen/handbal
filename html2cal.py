ca79d9ff94230bf05dd3c7357d7640a648d27ee9
Bert <bert.claesen+github@gmail.com>
2021-10-17 08:18:34 +0200

    Clean up code a bit

--- html2cal.py
+++ html2cal.py
@@ -10,62 +10,69 @@ from datetime  import datetime, timedelta
 from icalendar import Calendar, Event
 import pytz
 
-def tidyR(s):
-        return s.replace("(R)", "").strip()
+MY_TEAM = "Real Kiewit (R) "
+
+def tidyR(team_name):
+    """
+    Clean up the (R) in every team name, indicating it's the "Regio" Team
+    """
+    return team_name.replace("(R)", "").strip()
+
+def cells_to_game(cells):
+    """
+    maps the content of 6 <td>s to a calender object
+    """
+    match = {}
+    match["Date"] = cells[1].text
+    match["Location"] = cells[2].text
+    home_team = tidyR(cells[3].text)
+    away_team = tidyR(cells[5].text)
+    match["Game"] = f"{home_team}-{away_team}"
+    return match
 
 ###
 ###Get the matches
 ###
 doc = open("competitiekalender2021.html")
 soup = BeautifulSoup(doc, 'html.parser')
-soup.find_all('a')
-#elke tr met 6 kinderen
+
+#Every <tr> with 6 children is a game
 trs = soup.find_all('tr')
-matches = []
+games = []
 for t in trs:
     cells = t.find_all('td')
     if len(cells) == 6:
-        match = {}
-        match["Date"] = cells[1].text
-        match["Location"] = cells[2].text
-        match["Game"] = tidyR(cells[3].text) + "-" + tidyR(cells[5].text)
+        game = cells_to_game(cells)
+        games.append(game)
 
-        #match["Game"] = match["Game"].replace("(R)", "")
-        #match["Game"] = match["Game"].strip()
-        matches.append(match)
-        
+doc.close()
    
 ###
 ### Filter the matches
 ###
-matches = [m for m in matches if m["Game"].find(tidyR("Real Kiewit (R) ")) != -1 and m["Game"].find("bye ")==-1]
+games = [m for m in games if m["Game"].find(tidyR(MY_TEAM)) != -1 and m["Game"].find("bye ")==-1]
            
-#for m in matches:
-#    print("{}: {} // {}".format(m['Date'],m['Game'],m['Location']))
-    
 ###
 ### Create calendar
 ###
-#29 okt 2017 is DST terug weg
-#25 maart 2018 is DST er weer
 cal = Calendar()
 fmt = "%d/%m/%Y %H:%M"
 tzone = pytz.timezone("Europe/Brussels")
-dststart = datetime(2018,10,28,2)
-dstend = datetime(2019,3,31,2)
-for m in matches:
+
+for m in games:
     event = Event()
     event.add('summary', m["Game"])
-    dtstart = datetime.strptime(m["Date"],fmt)  
-    dtstart = tzone.localize(dtstart)
-    dtend = dtstart + timedelta(0,60*60)
-    event.add('dtstart', dtstart)
-    event.add('dtend', dtend)
-    #event.add('description', row['Activity'])
+    event_start = datetime.strptime(m["Date"],fmt)  
+    event_start = tzone.localize(event_start)
+    event_end = event_start + timedelta(0,60 * 60)
+    event.add('dtstart', event_start)
+    event.add('dtend', event_end)
     event.add('location', m["Location"])
     cal.add_component(event)
-    
-    #write it
+
+###    
+### Write to output file
+###
 f = open('handbal.ics', 'wb')
 f.write(cal.to_ical())
 f.close()
