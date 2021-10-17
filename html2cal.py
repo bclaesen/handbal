14d41f8a32ec52fcb46b8f026e88b7419e454c97
Bert <bert.claesen+github@gmail.com>
2021-10-17 08:45:43 +0200

    Refactoring

--- html2cal.py
+++ html2cal.py
@@ -6,39 +6,55 @@ Created on Sat Aug 26 11:29:07 2017
 @author: Bert
 """
 from bs4 import BeautifulSoup
-from datetime  import datetime, timedelta
+from dataclasses import dataclass
+from datetime import datetime, timedelta
 from icalendar import Calendar, Event
+from typing import List
 import pytz
 
 MY_TEAM = "Real Kiewit (R) "
 
-def tidyR(team_name):
-    """
-    Clean up the (R) in every team name, indicating it's the "Regio" Team
-    """
+
+@dataclass
+class Game:
+    """Keep track of game specific information."""
+    date: str
+    location: str
+    home_team: str
+    away_team: str
+
+    def description(self):
+        """Describes a game with its home and away team."""
+        return f"{self.home_team}-{self.away_team}"
+
+    def has_participant(self, team_name: str):
+        """Indicate if a team is playing in this game or not."""
+        return self.home_team == team_name or self.away_team == team_name
+
+
+def tidyR(team_name: str):
+    """Clean up the (R) in every team name, indicating it's the "Regio" Team."""
     return team_name.replace("(R)", "").strip()
 
+
 def cells_to_game(cells):
-    """
-    maps the content of 6 <td>s to a calender object
-    """
-    match = {}
-    match["Date"] = cells[1].text
-    match["Location"] = cells[2].text
+    """Maps the content of 6 <td>s to a calender object."""
+    date = cells[1].text
+    location = cells[2].text
     home_team = tidyR(cells[3].text)
     away_team = tidyR(cells[5].text)
-    match["Game"] = f"{home_team}-{away_team}"
-    return match
+    return Game(date, location, home_team, away_team)
+
 
 ###
-###Get the matches
+# Get the matches
 ###
 doc = open("competitiekalender2021.html")
 soup = BeautifulSoup(doc, 'html.parser')
 
-#Every <tr> with 6 children is a game
+# Every <tr> with 6 children is a game
 trs = soup.find_all('tr')
-games = []
+games: List[Game] = []
 for t in trs:
     cells = t.find_all('td')
     if len(cells) == 6:
@@ -46,32 +62,39 @@ for t in trs:
         games.append(game)
 
 doc.close()
-   
+
 ###
-### Filter the matches
+# Filter the matches
 ###
-games = [m for m in games if m["Game"].find(tidyR(MY_TEAM)) != -1 and m["Game"].find("bye ")==-1]
-           
+my_team = tidyR(MY_TEAM)
+games = [m for m in games if m.has_participant(
+    my_team) and not m.has_participant("bye ")]
+
 ###
-### Create calendar
+# Create calendar
 ###
+
+def parse_datetime(s: str, fmt: str, tz):
+    """Parse a string date to a timezone correct datetime"""
+    raw_dt = datetime.strptime(m.date, fmt)
+    return tzone.localize(raw_dt)
+
+
 cal = Calendar()
 fmt = "%d/%m/%Y %H:%M"
 tzone = pytz.timezone("Europe/Brussels")
 
 for m in games:
     event = Event()
-    event.add('summary', m["Game"])
-    event_start = datetime.strptime(m["Date"],fmt)  
-    event_start = tzone.localize(event_start)
-    event_end = event_start + timedelta(0,60 * 60)
+    event.add('summary', m.description())
+    event_start = parse_datetime(m.date, fmt, tzone)
     event.add('dtstart', event_start)
-    event.add('dtend', event_end)
-    event.add('location', m["Location"])
+    event.add('dtend', event_start + timedelta(0, 60 * 60))
+    event.add('location', m.location)
     cal.add_component(event)
 
-###    
-### Write to output file
+###
+# Write to output file
 ###
 f = open('handbal.ics', 'wb')
 f.write(cal.to_ical())
